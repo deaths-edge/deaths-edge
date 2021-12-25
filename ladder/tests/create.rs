16a0101494cd3ea9f643310978a9f2aaf90607d8
Harry Barber <harrybarber@protonmail.com>
2021-12-25 02:11:30 +0000

    Add create test

---
+++ ladder/tests/create.rs
@@ -0,0 +1,29 @@
+use std::env;
+
+use reqwest::Client;
+use serde_json::json;
+
+#[tokio::test]
+async fn test_create() {
+    dotenv::dotenv().ok();
+
+    let host = env::var("HOST").expect("HOST is not set in .env file");
+    let port = env::var("PORT").expect("PORT is not set in .env file");
+    let url = format!("http://{}:{}/create", host, port);
+
+    let client = Client::new();
+
+    let json = json!({
+        "name": "harry",
+        "username": "harry",
+        "password": "admin"
+    });
+    let response = client
+        .post(url)
+        .json(&json)
+        .send()
+        .await
+        .expect("request failed");
+
+    assert_eq!(response.status(), 200);
+}
